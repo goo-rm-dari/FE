4d1a6cb5eb01859ee02d82d7d921e99a4d8bcc42
Dipokal HHJ <hhj@devent.kr>
2024-05-22 23:30:11 +0900

    feat: get location

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,19 +1,54 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { Map, MapMarker } from "react-kakao-maps-sdk"
+import { geolocation } from "../utils/getLocation"
 
 export function MapPage() {
+    const [nowLocation, setNowLocation] = useState({
+        long: 33.5563,
+        lat: 126.79581,
+    })
 
+    const [locationList, setLocationList] = useState([{
+        long: 33.5563,
+        lat: 126.79581,
+    }])
+
+    const getGeoLocation = async () => {
+        const getLocation = await geolocation.get()
+        console.log(getLocation)
+        setNowLocation({
+            long: getLocation?.long,
+            lat: getLocation?.lat
+        })
+
+        locationList.push({
+            long: getLocation?.long,
+            lat: getLocation?.lat
+        })
+
+        setLocationList([...locationList])
+    }
+
+    useEffect(() => {
+        setInterval(() => {
+            getGeoLocation()
+
+        }, 1000)
+    }, [])
 
     return (
         <>
 
             <Map
-                center={{ lat: 33.5563, lng: 126.79581 }}
+                center={{ lat: nowLocation.lat, lng: nowLocation.long }}
                 style={{ width: "100%", height: "360px" }}
             >
-                <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-                    <div style={{ color: "#000" }}>Hello World!</div>
-                </MapMarker>
+                {locationList.map(geo => (
+                    <MapMarker position={{ lat: geo.lat, lng: geo.long }}>
+                        <div style={{ color: "#000" }}>Hello World!</div>
+                    </MapMarker>
+                ))}
+
             </Map>
         </>
 
