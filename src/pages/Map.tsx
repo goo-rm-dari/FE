93fe17a752b434da07c38fbd3c82814fca0ff371
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-05-23 17:10:47 +0900

    refactor: 맵 페이지 수정

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -7,40 +7,38 @@ import { Button } from '../components/Button';
 import { TopShadow } from '../components/TopShadow';
 import { geolocation } from '../utils/getLocation';
 
-export function MapPage() {
+const MIN_TIME_INTERVAL = 3000; // Minimum time interval in ms
+const MAX_BUFFER_SIZE = 5; // Adjust based on the desired smoothing level
+
+function MapPage() {
   const navigate = useNavigate();
 
-  const [firstLocation, setFirstLocation] = useState({
-    long: 33.5563,
-    lat: 126.79581,
-  });
-  const [nowLocation, setNowLocation] = useState({
-    long: 33.5563,
-    lat: 126.79581,
-  });
+  const [firstLocation, setFirstLocation] = useState<{
+    lat: number | null;
+    long: number | null;
+  }>({ lat: null, long: null });
+  const [nowLocation, setNowLocation] = useState<{
+    lat: number | null;
+    long: number | null;
+  }>({ lat: null, long: null });
+  const [locationList, setLocationList] = useState<
+    Array<{ lat: number; long: number }>
+  >([]);
+  const [trashLocationList, setTrashLocationList] = useState<
+    Array<{ lat: number; long: number }>
+  >([]);
 
-  const [locationList, setLocationList] = useState([
-    {
-      long: 33.5563,
-      lat: 126.79581,
-    },
-  ]);
-
-  const [trashLocationList, setTrashLocationList] = useState<any>([]);
-
-  const [moveLine, setMoveLine] = useState<any>();
+  const [moveLine, setMoveLine] = useState<any>(null);
 
   const [trashCount, setTrashCount] = useState(0);
   const [distance, setDistance] = useState(0);
   const [kcal, setKcal] = useState(0);
 
-  const [startTime, setStartTime] = useState(new Date());
-  const [savedDuration, setSavedDuration] = useState(0);
   const [duration, setDuration] = useState(0);
   const [isClock, setIsClock] = useState(true);
   const timeRef = useRef<any>(null);
 
-  const interval = 1000;
+  const interval = 3000; // Set to 3 seconds for recording coordinates
   const markerImage = '/marker.png';
   const trashMarkerImage = '/trash.jpeg';
 
@@ -49,12 +47,13 @@ export function MapPage() {
   const spriteSize = { width: imageShowSize, height: imageShowSize };
   const storeOrigin = { x: 0, y: 0 };
 
-  const locationBuffer = useRef([]);
-  const lastUpdateTime = useRef(new Date().getTime());
-  const MAX_BUFFER_SIZE = 5; // Adjust based on the desired smoothing level
-  const MIN_TIME_INTERVAL = 5000; // Minimum time interval in ms
+  const locationBuffer = useRef<Array<{ lat: number; long: number }>>([]);
+  const lastUpdateTime = useRef<number>(new Date().getTime());
 
-  const calculateDistance = (coord1, coord2) => {
+  const calculateDistance = (
+    coord1: { lat: number; long: number },
+    coord2: { lat: number; long: number },
+  ) => {
     const R = 6371; // Radius of the Earth in km
     const dLat = (coord2.lat - coord1.lat) * (Math.PI / 180);
     const dLon = (coord2.long - coord1.long) * (Math.PI / 180);
@@ -70,38 +69,47 @@ export function MapPage() {
 
   const getFirstLocation = async () => {
     const getLocation = await geolocation.get();
-    setLocationList([]);
-
-    setFirstLocation({
-      long: getLocation?.long,
-      lat: getLocation?.lat,
-    });
+    if (getLocation) {
+      setFirstLocation({
+        long: getLocation.long,
+        lat: getLocation.lat,
+      });
+      setLocationList([
+        {
+          long: getLocation.long,
+          lat: getLocation.lat,
+        },
+      ]);
+    }
   };
 
   const getGeoLocation = async () => {
     const getLocation = await geolocation.get();
-    const nowTime = new Date().getTime();
-    if (
-      calculateDistance(locationList[locationList.length - 1], getLocation) <
-        50 &&
-      nowTime - lastUpdateTime.current > MIN_TIME_INTERVAL
-    ) {
-      locationBuffer.current.push(getLocation);
-      if (locationBuffer.current.length > MAX_BUFFER_SIZE) {
-        locationBuffer.current.shift();
+    if (getLocation && locationList.length > 0) {
+      const nowTime = new Date().getTime();
+      if (
+        calculateDistance(locationList[locationList.length - 1], getLocation) >=
+          50 &&
+        nowTime - lastUpdateTime.current > MIN_TIME_INTERVAL
+      ) {
+        setNowLocation(getLocation);
+        locationBuffer.current.push(getLocation);
+        if (locationBuffer.current.length > MAX_BUFFER_SIZE) {
+          locationBuffer.current.shift();
+        }
+        const averageLocation = locationBuffer.current.reduce(
+          (acc, loc) => {
+            acc.lat += loc.lat;
+            acc.long += loc.long;
+            return acc;
+          },
+          { lat: 0, long: 0 },
+        );
+        averageLocation.lat /= locationBuffer.current.length;
+        averageLocation.long /= locationBuffer.current.length;
+        setNowLocation(averageLocation);
+        lastUpdateTime.current = nowTime;
       }
-      const averageLocation = locationBuffer.current.reduce(
-        (acc, loc) => {
-          acc.lat += loc.lat;
-          acc.long += loc.long;
-          return acc;
-        },
-        { lat: 0, long: 0 },
-      );
-      averageLocation.lat /= locationBuffer.current.length;
-      averageLocation.long /= locationBuffer.current.length;
-      setNowLocation(averageLocation);
-      lastUpdateTime.current = nowTime;
     }
   };
 
@@ -111,13 +119,15 @@ export function MapPage() {
 
   const addTrash = async () => {
     const getLocation = await geolocation.get();
-    setTrashLocationList([
-      ...trashLocationList,
-      {
-        long: getLocation?.long,
-        lat: getLocation?.lat,
-      },
-    ]);
+    if (getLocation) {
+      setTrashLocationList([
+        ...trashLocationList,
+        {
+          long: getLocation.long,
+          lat: getLocation.lat,
+        },
+      ]);
+    }
   };
 
   const calculateKcal = () => {
@@ -126,12 +136,8 @@ export function MapPage() {
   };
 
   const startTimer = () => {
-    setStartTime(() => new Date());
-    setSavedDuration(0);
-
     timeRef.current = setInterval(() => {
       setDuration(time => time + 1000);
-      console.log('AAAA', savedDuration);
     }, 1000);
   };
 
@@ -145,36 +151,45 @@ export function MapPage() {
   };
 
   const handleClickStopPlogging = () => {
+    const createPloggingForm = {
+      locationList,
+      timer:
+        Math.floor(duration / 60000) +
+        ':' +
+        ((duration % 60000) / 1000).toFixed(0).padStart(2, '0'),
+      distance: (distance / 1000).toFixed(2),
+      kcal: kcal,
+    };
+    console.log(createPloggingForm);
+    clearInterval(timeRef.current);
     navigate('/plogging/done');
   };
 
   const handleClickStopClock = () => {
-    setStartTime(() => new Date());
-    setSavedDuration(
-      saved => saved + (new Date().getTime() - startTime.getTime()),
-    );
-    setIsClock(false);
     stopTimer();
+    setIsClock(false);
   };
 
   const handleClickStartClock = () => {
-    setStartTime(() => new Date());
     setIsClock(true);
     startTimer();
   };
 
   useEffect(() => {
-    if (
-      locationList.length > 0 &&
-      calculateDistance(locationList[locationList.length - 1], nowLocation) < 50
-    ) {
-      setLocationList([...locationList, nowLocation]);
-    }
+    if (firstLocation.lat !== null && nowLocation.lat !== null) {
+      if (
+        locationList.length > 0 &&
+        calculateDistance(locationList[locationList.length - 1], nowLocation) <
+          50
+      ) {
+        setLocationList([...locationList, nowLocation]);
+      }
 
-    try {
-      setDistance(getGeoLocationLength());
-      setKcal(calculateKcal());
-    } catch (error) {}
+      try {
+        setDistance(getGeoLocationLength());
+        setKcal(calculateKcal());
+      } catch (error) {}
+    }
   }, [nowLocation]);
 
   useEffect(() => {
@@ -191,58 +206,72 @@ export function MapPage() {
     };
   }, []);
 
+  useEffect(() => {
+    const initializeLocation = async () => {
+      await getFirstLocation();
+      setNowLocation(await geolocation.get());
+    };
+
+    initializeLocation();
+  }, []);
+
   return (
     <>
-      <Map
-        center={{ lat: firstLocation.lat, lng: firstLocation.long }}
-        style={{ width: '100%', height: '50vh' }}
-      >
-        <Polyline
-          path={[
-            locationList.map(item => {
-              return { lat: item.lat, lng: item.long };
-            }),
-          ]}
-          strokeWeight={10}
-          strokeColor={'#7FD6E1'}
-          strokeOpacity={1}
-          strokeStyle={'solid'}
-          onCreate={setMoveLine}
-        />
-
-        <MapMarker
-          position={{ lat: nowLocation.lat, lng: nowLocation.long }}
-          image={{
-            src: markerImage,
-            size: imageSize,
-            options: {
-              spriteSize: spriteSize,
-              spriteOrigin: storeOrigin,
-            },
+      {firstLocation.lat !== null && firstLocation.long !== null && (
+        <Map
+          center={{
+            lat: nowLocation.lat || firstLocation.lat,
+            lng: nowLocation.long || firstLocation.long,
           }}
-        />
-
-        {trashLocationList.map((trash: any, index: number) => (
-          <MapMarker
-            key={index}
-            position={{ lat: trash.lat, lng: trash.long }}
-            image={{
-              src: trashMarkerImage,
-              size: imageSize,
-              options: {
-                spriteSize: spriteSize,
-                spriteOrigin: storeOrigin,
-              },
-            }}
+          style={{ width: '100%', height: '50vh' }}
+        >
+          <Polyline
+            path={[
+              locationList.map(item => {
+                return { lat: item.lat, lng: item.long };
+              }),
+            ]}
+            strokeWeight={10}
+            strokeColor={'#7FD6E1'}
+            strokeOpacity={1}
+            strokeStyle={'solid'}
+            onCreate={setMoveLine}
           />
-        ))}
-      </Map>
+
+          {nowLocation.lat !== null && nowLocation.long !== null && (
+            <MapMarker
+              position={{ lat: nowLocation.lat, lng: nowLocation.long }}
+              image={{
+                src: markerImage,
+                size: imageSize,
+                options: {
+                  spriteSize: spriteSize,
+                  spriteOrigin: storeOrigin,
+                },
+              }}
+            />
+          )}
+
+          {trashLocationList.map((trash, index) => (
+            <MapMarker
+              key={index}
+              position={{ lat: trash.lat, lng: trash.long }}
+              image={{
+                src: trashMarkerImage,
+                size: imageSize,
+                options: {
+                  spriteSize: spriteSize,
+                  spriteOrigin: storeOrigin,
+                },
+              }}
+            />
+          ))}
+        </Map>
+      )}
 
       <b className='absolute left-2 top-2 z-10'>{trashCount}개의 쓰레기</b>
-
       <div className='relative bottom-0 left-0 z-10 h-1/2 w-full bg-white'>
         <TopShadow />
-
         <div className='flex pt-2'>
           <div className='flex flex-1 justify-center'>
             <b>{kcal}kcal</b>
@@ -251,12 +280,11 @@ export function MapPage() {
             <b>
               {Math.floor(duration / 60000) +
                 ':' +
-                (duration < 10 ? '0' : '') +
-                ((duration % 60000) / 1000).toFixed(0)}
+                ((duration % 60000) / 1000).toFixed(0).padStart(2, '0')}
             </b>
           </div>
           <div className='flex flex-1 justify-center'>
-            <b>{distance / 1000}km</b>
+            <b>{(distance / 1000).toFixed(2)}km</b>
           </div>
         </div>
 
@@ -279,3 +307,5 @@ export function MapPage() {
     </>
   );
 }
+
+export default MapPage;
