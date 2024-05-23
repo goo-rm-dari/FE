1960de65d8570c98bdd6a9dfc52f6d14856dc83a
Dipokal HHJ <hhj@devent.kr>
2024-05-23 11:12:11 +0900

    feat: add load page

--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import { GyroscopePage } from './pages/Gyroscope';
 import LoginPage from './pages/login';
 import SignInPage from './pages/signIn';
+import { CountdownPage } from './pages/Countdown';
 
 export const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ export const router = createBrowserRouter([
   {
     path: '/test',
     element: <GyroscopePage />,
+  },
+  {
+    path: '/load',
+    element: <CountdownPage />,
   },
   {
     path: '/login',
